3a41849ead49d8efb0f55146eba4e2a101e39db8
Devarat Patel <devaratpatel1@gmail.com>
2021-12-30 16:28:47 -0500

    tournament winner

--- arrays/easy/arrays.js
+++ arrays/easy/arrays.js
@@ -134,3 +134,39 @@ function sortedSquaredArray(array) {
   }
   return sortedSquares;
 }
+
+/* <--------------------------------------------> */
+
+// Problem 4 - Tournament Winner
+
+// Solution 1: (Hash Table, Helper Function)
+// 0(n) time | 0(k) space
+
+const HOME_TEAM_WON = 1;
+
+function tournamentWinner(competitions, results) {
+  // Write your code here.
+	let currentBestTeam = "";
+	const scores = {[currentBestTeam] : 0};
+
+	for (let i = 0; i < competitions.length; i++) {
+		const result = results[i];
+		const [homeTeam, awayTeam] = competitions[i];
+
+		const winningTeam = result === HOME_TEAM_WON ? homeTeam : awayTeam;
+
+		updateScores(winningTeam, 3, scores);
+
+		if (scores[winningTeam] > scores[currentBestTeam]) {
+			currentBestTeam = winningTeam;
+		}
+	}
+  return currentBestTeam;
+}
+
+function updateScores(team, points, scores) {
+	if (!(team in scores)) scores[team] = 0;
+
+	scores[team] += points;
+}
+
